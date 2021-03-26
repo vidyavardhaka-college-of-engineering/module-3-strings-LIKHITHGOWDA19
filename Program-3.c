8a6d7647c176bcd205d72505764aa5a62d84fa4e
LIKHITHGOWDA19 <likhi192002@gmail.com>
2021-03-26 11:15:36 +0000

    done

--- Program-3.c
+++ Program-3.c
@@ -18,16 +18,16 @@ int main() {
 	scanf("%s", a);
 	printf("Enter the second string : ");
 	scanf("%s", b);
-	for ( )                                                // Complete the code in for
+	for (i=0;a[i]!='\0';i++ )                                                // Complete the code in for
            { 
-		c[i] = ;                                              //Complete the statement
+		c[i] = a[i];                                              //Complete the statement
 	}
-	for ( )                                                 // Complete the code in for
-           { 
-		c[i] = ;                                              //Complete the statement
+	for (j=0; b[j]!='\0';j++ )                                  {
+
+		c[i] = b[j];                                              //Complete the statement
 		i++;
 	}
-	c[i]= ;                                               //Complete the statement
-	printf("The concatenated string = %s", c);
+	c[i]='\0' ;                                               //Complete the statement
+	printf("The concatenated string = %s\n", c);
 	return 0;
 }
